61c931ce15d9c23edfbc24d3c0ccc796b6fbc9f6
Xtreme_Light <whateverxl@outlook.com>
2022-07-06 00:09:22 +0800

    init

--- src/command.rs
+++ src/command.rs
@@ -1,8 +1,73 @@
 #[cfg(test)]
 mod test {
+    use crate::command::{AddField, CreateTable, Schema};
 
     #[test]
     pub fn test_() {
-        assert_eq!(1, 1);
+        let mut schema = Schema::new();
+
+        let cmd = Box::new(CreateTable);
+        schema.add_migration(cmd);
+        let cmd = Box::new(AddField);
+        schema.add_migration(cmd);
+
+        assert_eq!(vec!["create table", "add field"], schema.execute());
+        assert_eq!(vec!["remove field", "drop table"], schema.rollback());
+    }
+}
+
+/// 所有命令必须实现的两个动作
+pub trait Migration {
+    fn execute(&self) -> &str;
+    fn rollback(&self) -> &str;
+}
+
+pub struct CreateTable;
+
+impl Migration for CreateTable {
+    fn execute(&self) -> &str {
+        "create table"
+    }
+
+    fn rollback(&self) -> &str {
+        "drop table"
+    }
+}
+
+pub struct AddField;
+
+impl Migration for AddField {
+    fn execute(&self) -> &str {
+        "add field"
+    }
+
+    fn rollback(&self) -> &str {
+        "remove field"
+    }
+}
+
+struct Schema {
+    commands: Vec<Box<dyn Migration>>,
+}
+
+impl Schema {
+    fn new() -> Self {
+        Self { commands: vec![] }
+    }
+
+    fn add_migration(&mut self, cmd: Box<dyn Migration>) {
+        self.commands.push(cmd);
+    }
+
+    fn execute(&self) -> Vec<&str> {
+        self.commands.iter().map(|cmd| cmd.execute()).collect()
+    }
+
+    fn rollback(&self) -> Vec<&str> {
+        self.commands
+            .iter()
+            .rev()
+            .map(|cmd| cmd.rollback())
+            .collect()
     }
 }
